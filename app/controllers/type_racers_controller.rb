25a3e1eff66746c59bf3a51084ffadacead0d80e
Nihaal Dhakal <nihaldhakal41@gmail.com>
2019-10-15 11:01:41 +0545

    Added bootstrap and devise gem

---
+++ app/controllers/type_racers_controller.rb
@@ -0,0 +1,7 @@
+class TypeRacersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
---
+++ test/models/type_racer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TypeRacerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
